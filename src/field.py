d9069be0ece00092833ca0e1fc597df41897bd7a
VAVASTO <vladimir.polotsk@gmail.com>
2022-12-08 21:14:20 +0300

    hw done

---
+++ src/field.py
@@ -0,0 +1,23 @@
+goods = [
+    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
+    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
+ ]
+
+def field(items, *args):
+    
+    assert len(args) > 0
+    if len(args) == 1:
+        for i in items:        
+            yield i[args[0]]
+        
+    else:
+        for i in items:
+            yield {key:i[key] for key in args}
+
+
+ans = [i for i in field(goods, 'title', 'color')]
+print(ans)
+#print(field(goods, 'title'))
+
+
+
